f0b288002e8b8a2ff4d8b90074168a5cc05ad55e
Donald Kruse <dkmelodee@gmail.com>
2019-01-14 15:36:55 -0700

    added implimentation for constructor and destructor

--- src/Graph.h
+++ src/Graph.h
@@ -8,7 +8,7 @@
 #include "Person.h"
 
 
-/* a Vertex that is used in the Graph class
+/* A Vertex that is used in the Graph class
  *
  * Should only contain public data and no
  * information about neighbors, connections, etc.
@@ -27,21 +27,23 @@ struct Vertex {
            Person * vperson,
            std::string vname) :
 
-           label (vlabel),
-           is_chair (vis_chair),
-           is_occupied (vis_occupied),
-           person (vperson),
-           name (vname)
+           label(vlabel),
+           is_chair(vis_chair),
+           is_occupied(vis_occupied),
+           person(vperson),
+           name(vname)
            {}
 };
 
-// adjacency list for a single Vertex 
+
+// adjacency list for a single Vertex
 typedef std::pair< Vertex, std::vector<Vertex> > AdjacencyList;
 
 
 // This is the actual representation of the graph -- a list of adjacency lists.
-// Order somewhat matters. May change later.
-typedef std::vector< Vertex, adjacency_list > GraphRepresenter;
+//
+// This may need to be changed to an unordered map for efficiency later.
+typedef std::vector< AdjacencyList > GraphRepresenter;
 
 
 // TODO(DonaldKruse):
@@ -49,33 +51,27 @@ typedef std::vector< Vertex, adjacency_list > GraphRepresenter;
 class Graph {
     private:
         GraphRepresenter _graph;
-        
+
         // basic info for size and other metrics
         unsigned int _num_nodes;
         unsigned int _num_chairs;
         unsigned int _num_chairs_occupied;
         unsigned int _num_persons; // number of Person objs that exists in the  graph
-        
-        // TODO(DonaldKruse):
-        // union and intersection methods (if needed) should be private
-        
-        // TODO(DonaldKruse):
-        // update on number of nodes, chairs, persons, etc...
-        
-
-    public:
-        // general constructors
-        //
         Graph();
-        Graph(GraphRepresenter & graph);
+    public:
+
+        // general constructor
+        Graph(GraphRepresenter graph);
 
         // getters and setters for vertices, edges
         //
-        AdjacencyList * get_adjlist ();    // dangerous
-        void get_num_nodes ();
-        void get_num_chairs ();
-        void get_num_chairs_occupied ();
-        void get_num_persons ();
+        void get_num_nodes();
+        void get_num_chairs();
+        void get_num_chairs_occupied();
+        void get_num_persons();
+
+        void get_nodes();
+
 
         // TODO(DonaldKruse): check to make sure this is an adequate prototype
         /* add_adjlist_to_graph
@@ -83,35 +79,37 @@ class Graph {
          * input new_adjlists: a GraphRepresenter that will be merged
          *                    with this Graph's _graph
          *
-         * output: void  
+         * output: void
          *
          * exceptions: TODO(DonaldKruse):
          *
          * Notes: This method does not check for any correctness of
-         *        of the graph, but copies the contents of 
+         *        of the graph, but copies the contents of
          *        new_adjlist to this._graph then performs
          *        and update on the number of nodes, persons, etc...
          *
          */
         //void add_adjlists_to_graph(GraphRepresenter new_adjlists);
 
-        
+
         // TODO(DonaldKruse): ops on single given node in graph
-        //    list of neighbors
-        //    what kind of node
+        std::vector<Vertex> get_neighbors(Vertex vertex);
+
 
         // TODO(DonaldKruse): test for adjacency between two nodes
-    
+        bool are_neighbors(Vertex vertex1, Vertex vertex2);
+
+
         // TODO(DonaldKruse): insert and delete for nodes and edges
-     
+        void insert_adjlist(AdjacencyList adjlist);
+        void delete_adjlist(AdjacencyList adjlist);
+
+
         // TODO(DonaldKruse): determine how and when to use a sparse matrix/data struct
         //      for example std::unordered_map has average constant-time complexity
         //      for lookup, insert, etc...
 
-        // TODO(DonaldKruse): methods for intersection and unions of graphs
- 
-        // TODO(DonaldKruse): destructor
         ~Graph();
-}
+};
 
 #endif // GRAPH_H_
