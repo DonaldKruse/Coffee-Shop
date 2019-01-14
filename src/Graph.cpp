5d320249041e8142f7baced4dcdda4f66e75e136
Donald Kruse <dkmelodee@gmail.com>
2019-01-14 15:36:55 -0700

    added implimentation for constructor and destructor

--- src/Graph.cpp
+++ src/Graph.cpp
@@ -4,6 +4,33 @@
 #include <string>
 
 
-
 // -- Constructor --
 //
+Graph::Graph(GraphRepresenter graph) {
+    _graph = graph;
+
+    // get number of nodes
+    _num_nodes = _graph.size();
+
+    // get number of chairs, occupied chairs, and persons
+    unsigned int numchairs   = 0;
+    unsigned int numoccupied = 0;
+    unsigned int numpersons  = 0;
+    for ( auto it = _graph.begin(); it != _graph.begin(); it++ ) {
+        auto fst = it->first;
+
+        if ( fst.is_chair )
+            numchairs++;
+
+        if ( fst.is_occupied )
+            numoccupied++;
+
+        if ( fst.person == nullptr )
+            numpersons++;
+    }
+    _num_chairs = numchairs;
+    _num_chairs_occupied = numoccupied;
+    _num_persons = numpersons;
+}
+
+Graph::~Graph() {}
