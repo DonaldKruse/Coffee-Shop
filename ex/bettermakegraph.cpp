51372842134a31a8ce827761ae910a511431fdc6
Donald Kruse <dkmelodee@gmail.com>
2019-01-17 09:55:41 -0700

    added operator overloading for std::find and std::sort in example

--- ex/bettermakegraph.cpp
+++ ex/bettermakegraph.cpp
@@ -1,8 +1,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
-#include <unordered_map>
-#include <map>
+#include <algorithm>
 #include <utility> // std::pair
 
 
@@ -29,13 +28,21 @@ struct Vertex {
            name (vname)
            {}
 
-    // operator overloading -- necessary if the struct is a key in a map
-    //bool const operator==(const Vertex &v) const {
-    //    return label == v.label;
-    //}
 
-    bool const operator<(const Vertex &v) const {
-        return label < v.label;
+    // -- operator overloading --
+
+
+    //   std::sort
+    bool operator<(const Vertex &v) const {
+        return (label < v.label);
+    }
+    
+    //   std::find
+    bool operator==(const Vertex &v) const {
+        return (label == v.label);
+    }
+    bool operator==(const unsigned int l) const {
+        return (label == l);
     }
 };
 
@@ -67,6 +74,39 @@ int main() {
         auto val1 = it->first;
         auto val2 = it->second;
         std::cout << "first: " << val1.label << "; second (get size of vector): " << val2.size() << std::endl;
+    }
+
+    // use std::sort on a vector of structs
+    Vertex v10(10, false, false, "");
+    Vertex v11(11, false, false, "");
+    Vertex v100(100, false, false, "");
+    std::vector<Vertex> vec = {v100, v11, v10};
+
+    std::cout << "Before sort, vec: ";
+    std::vector<Vertex>::iterator iter;
+    for (iter = vec.begin(); iter != vec.end(); iter++)
+        std::cout << iter->label << " ";
+    std::cout << std::endl;
+
+    std::cout << "After sort..." << std::endl;
+    std::sort(vec.begin(), vec.end());
+    std::cout << "vec: ";
+    for (iter = vec.begin(); iter != vec.end(); iter++)
+        std::cout << iter->label << " ";
+    std::cout << std::endl;
+
+    // using std::find on vec
+    std::cout << "Using std::find to find the value '11' in vec..." << std::endl;
+    int findval = 11;
+    auto result = std::find(vec.begin(), vec.end(), findval);
+    if (result != vec.end()) {
+        std::cout << "Vec contains " << findval << "!" << std::endl;
+        // use vec.begin() to find index and ptrdiff_t to avoid 
+        // negative values
+        ptrdiff_t index = result - vec.begin();
+        std::cout << "index = " << index << std::endl;
+    } else {
+        std::cout << "Vec does NOT contains " << findval << "!" << std::endl;
     }
     return 0;
 }
