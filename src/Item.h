c86e0410affb6e3ea7cf0c69de6dc19f817acc2d
Donald Kruse <dkmelodee@gmail.com>
2019-01-17 19:56:41 -0700

    cleaned up Item.h

--- src/Item.h
+++ src/Item.h
@@ -1,9 +1,15 @@
-#ifndef ITEM_H
-#define ITEM_H
+#ifndef ITEM_H_
+#define ITEM_H_
 
 #include <iostream>
 #include <string>
 
+
+/*
+ * Basic class for an item.
+ *
+ * May actually need to be a struct. Maybe not...
+ */
 class Item {
     private:
         std::string _name;
@@ -11,30 +17,39 @@ class Item {
         double _price;
         double _work;
         Item();
+
     public:
         // constructors
-        Item ( const std::string name,
-                const std::string description,
-                double price,
-                const double work);
+        Item(const std::string name,
+             const std::string description,
+             double price,
+             const double work);
 
-        Item( const Item &); // copy constructor
+        Item(const Item &);  // copy constructor
+
 
         // getters
         std::string get_name() const;
         std::string get_description() const;
-        double get_price() const { return _price; }
-        double get_work() const { return _work; }
+        double get_price() const {
+            return _price;
+        }
+        double get_work() const {
+            return _work;
+        }
+
 
         // setters
-        void set_description(std::string description) { 
+        void set_description(std::string description) {
             _description = description;
         }
 
-        void set_price( double price) { _price = price; }
+        void set_price(double price) {
+            _price = price;
+        }
 
-        // destructor
+
         ~Item();
 };
 
-#endif // ITEM_H
+#endif  // ITEM_H_
